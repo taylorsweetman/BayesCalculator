31d02573a6631497f63cdcc658aee6269bc29dd3
taylo <taylo@192.168.0.14>
2018-12-22 20:45:31 -0500

    refactored Event class to have text fields as an instance variable

--- Bayes Calculator/src/calculator/ui/CalculateListener.java
+++ Bayes Calculator/src/calculator/ui/CalculateListener.java
@@ -13,10 +13,6 @@ import calculator.domain.*;
 
 public class CalculateListener implements ActionListener {
 
-	private JTextField aGivenBText;
-	private JTextField aText;
-	private JTextField bText;
-	private JTextField bGivenAText;
 	private JButton calcButton;
 	private Calculator calcs;
 	private ParseTextToDouble parser;
@@ -24,64 +20,72 @@ public class CalculateListener implements ActionListener {
 	private Event a;
 	private Event b;
 	private Event bGivenA;
+	private List<Event> allEventsList;
 
 	public CalculateListener(JTextField aGivenB, JTextField a, JTextField b, JTextField bGivenA, JButton calcButton) {
-		this.aGivenBText = aGivenB;
-		this.aText = a;
-		this.bText = b;
-		this.bGivenAText = bGivenA;
+		this.aGivenB = new Event(aGivenB);
+		this.a = new Event(a);
+		this.b = new Event(b);
+		this.bGivenA = new Event(bGivenA);
 		this.calcButton = calcButton;
 		parser = new ParseTextToDouble();
 		calcs = new Calculator();
+		allEventsList = new ArrayList<Event>();
 	}
-	
-	public void fieldAction(JTextField inputField, Event inputEvent) {
+
+	public void fieldAction(Event inputEvent) {
 		double fieldsValue;
-		
+
 		try {
-			fieldsValue = parser.parse(inputField.getText());
-			inputEvent = new Event(fieldsValue);
+			fieldsValue = parser.parse(inputEvent.getTextField().getText());
+			inputEvent.setProb(fieldsValue);
+			allEventsList.add(inputEvent);
 		} catch (NumberFormatException nfe) {
-			inputField.setText("bad input bruh");
+			inputEvent.setText("bad input bruh");
 		} catch (EmptyCellException ece) {
-			inputEvent = new Event();
+
 		}
 	}
 
 	public List<Event> getUninitalizedEvents() {
 		ArrayList<Event> listToReturn = new ArrayList<Event>();
-		
+
 		if (!aGivenB.isInitialized()) {
 			listToReturn.add(aGivenB);
 		}
-		
+
 		if (!a.isInitialized()) {
 			listToReturn.add(a);
 		}
-		
+
 		if (!b.isInitialized()) {
 			listToReturn.add(b);
 		}
-		
+
 		if (!bGivenA.isInitialized()) {
 			listToReturn.add(bGivenA);
 		}
-		
+
 		return listToReturn;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent calcButtonHit) {
-		
-		fieldAction(aGivenBText, aGivenB);
-		fieldAction(aText, a);
-		fieldAction(bText, b);
-		fieldAction(bGivenAText, bGivenA);
-		
-		//refactor to tie the text fields to an event so that I can easily add an error message to the uninitialized events
+
+		fieldAction(aGivenB);
+		fieldAction(a);
+		fieldAction(b);
+		fieldAction(bGivenA);
+
+		// refactor to tie the text fields to an event so that I can easily add an error
+		// message to the uninitialized events
 		if (getUninitalizedEvents().size() > 1) {
-			for (Event event:getUninitalizedEvents()) {
-				
+			for (Event event : getUninitalizedEvents()) {
+				event.setText("only one event can not have prob set");
+			}
+		} else if (getUninitalizedEvents().size() == 0) {
+			for (Event event : getUninitalizedEvents()) {
+				event.setText("one field must remain blank");
 			}
 		}
 
